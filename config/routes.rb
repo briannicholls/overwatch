b7b76914c5d89b92e480aecea35518bc002ebe35
Brian <nichol88@gmail.com>
2022-06-14 02:54:01 -0400

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  root to: 'overwatch#index'
+  resources :roles
+  resources :heros
+
+  resources :hard_counters
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
