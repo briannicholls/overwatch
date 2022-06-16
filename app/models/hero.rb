31cedc197de31e1c4d2db544075b4cfd19e60f00
Brian <nichol88@gmail.com>
2022-06-15 20:56:26 -0400

    accept nested attributes for hard counters

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -2,8 +2,10 @@ class Hero < ApplicationRecord
   belongs_to :role
   
   has_many :hard_counters
-  has_many :advantage_heros,    class_name: 'Hero', foreign_key: :advantage_hero_id    , through: :hard_counters
-  has_many :disadvantage_heros, class_name: 'Hero', foreign_key: :disadvantage_hero_id , through: :hard_counters
+  has_many :advantage_heros,    class_name: 'Hero', foreign_key: :advantage_hero_id, through: :hard_counters
+  has_many :disadvantage_heros, class_name: 'Hero', foreign_key: :hero_id , through: :hard_counters
+
+  accepts_nested_attributes_for :hard_counters
 
   DVA = Hero.find(7)
   MEI = Hero.find(23)
