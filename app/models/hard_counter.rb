6b393407e120033574990fd69e37902e62ce9f83
Brian <nichol88@gmail.com>
2022-06-15 20:56:09 -0400

    fix associations

--- app/models/hard_counter.rb
+++ app/models/hard_counter.rb
@@ -1,5 +1,5 @@
 class HardCounter < ApplicationRecord
-  belongs_to :hero
+  # belongs_to :hero
   belongs_to :advantage_hero, class_name: 'Hero', foreign_key: :advantage_hero_id
-  # belongs_to :disadvantage_hero, class_name: 'Hero', foreign_key: :disadvantage_hero_id
+  belongs_to :disadvantage_hero, class_name: 'Hero', foreign_key: :hero_id # will always return own hero due to relationship in hard_counters. use hard_counters instead
 end
