60fe1df115813f2b041e738f55aaa2b02e93a678
Brian <nichol88@gmail.com>
2022-06-24 12:52:42 -0400

    add ability model

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -4,7 +4,7 @@ class Hero < ApplicationRecord
   has_many :hard_counters
   has_many :advantage_heros,    class_name: 'Hero', foreign_key: :advantage_hero_id, through: :hard_counters
   has_many :disadvantage_heros, class_name: 'Hero', foreign_key: :hero_id , through: :hard_counters
-
+  has_many :abilities
   accepts_nested_attributes_for :hard_counters
 
   def advantage_hero_ids=(ids)
