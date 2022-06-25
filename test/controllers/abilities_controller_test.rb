3808f8eb7c03b2c0fe0937dbd144960f666f8522
Brian <nichol88@gmail.com>
2022-06-24 23:12:25 -0400

    add abilities view

---
+++ test/controllers/abilities_controller_test.rb
@@ -0,0 +1,33 @@
+require "test_helper"
+
+class AbilitiesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get abilities_index_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get abilities_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get abilities_create_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get abilities_show_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get abilities_destroy_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get abilities_update_url
+    assert_response :success
+  end
+end
