9b07fec9a28cbc1132ecb492cfa3c177e743e90d
Brian <nichol88@gmail.com>
2022-07-01 17:46:51 -0400

    run pending migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_30_064004) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_30_065805) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -73,6 +73,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_30_064004) do
     t.boolean "applies_speed_penalty_enemy", default: false
     t.float "min_boop_distance"
     t.boolean "shares_ammo_with_primary"
+    t.integer "number_of_projectiles"
     t.index ["hero_id"], name: "index_abilities_on_hero_id"
   end
 
