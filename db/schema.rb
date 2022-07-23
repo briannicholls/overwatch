4eb3e693c0723113970c9b69ae1a74987d1cc8f3
Brian <nichol88@gmail.com>
2022-07-23 18:07:17 -0400

    add shields to abilities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_23_220532) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_23_220658) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -76,6 +76,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_23_220532) do
     t.integer "number_of_projectiles"
     t.boolean "ignores_barriers", default: false
     t.boolean "is_hitscan", default: false
+    t.integer "shields"
     t.index ["hero_id"], name: "index_abilities_on_hero_id"
   end
 
