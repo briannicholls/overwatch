8ad3549a58e12802a38586abd978c50858e6fdf1
Brian <nichol88@gmail.com>
2022-08-09 21:01:24 -0400

    improve hero show page;

--- spec/models/hero_spec.rb
+++ spec/models/hero_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Hero, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
+
+  
   
   describe ".all" do
-    it "should return all valid heroes" do
+    it "should return valid heroes" do
       Hero.all.all?(&:valid?)
     end
   end
