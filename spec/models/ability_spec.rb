86092903a69d55520e738e722b6f0a94e7b5f1bf
Brian Nicholls <nichol88@gmail.com>
2022-09-11 18:19:02 -0400

    refactor spec for clarity - test each ability

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe Ability, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
 
   describe "dps" do
-    before do
+    context "for Abilities that deal damage"
+    it "should calculate a valid DPS value" do
       @abilities = Ability.damage_dealing
-    end
-    context "if this ability does damage"
-    it "should return a valid dps" do
-      # binding.pry
-      expect(@abilities.map(&:dps).all?(&:present?)).to be true
+      @abilities.each do |ability|
+        puts "Testing #{ability.hero.name}'s Ability: #{ability.name} (#{ability.keybinding})"
+        expect(ability.dps.present?).to be true
+      end
     end
   end
 
