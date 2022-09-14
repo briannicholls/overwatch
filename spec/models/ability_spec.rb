9143df0920a37fe9276247141a09b86cb11a6a63
Brian Nicholls <nichol88@gmail.com>
2022-09-13 23:09:08 -0400

    refactor tests to test each ability individually

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -1,17 +1,15 @@
 require 'rails_helper'
 require 'pry'
-RSpec.describe Ability, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
 
-  describe "#dps" do
-    context "for Abilities that deal damage"
-    it "should calculate a valid DPS value" do
-      @abilities = Ability.non_ultimates.damage_dealing
-      @abilities.each do |ability|
-        puts "Testing #{ability.hero.name}'s Ability: #{ability.name} (#{ability.keybinding})"
-        expect(ability.dps.present?).to be true
-      end
+RSpec.describe Ability, type: :model do
+  context "for Abilities that deal damage"
+    @abilities = Ability.non_ultimates.damage_dealing
+    @abilities.each do |ability|
+      describe "#{ability.name}#dps" do
+        it "should calculate a valid DPS value" do
+          puts "Testing #{ability.hero.name}'s Ability: #{ability.name} (#{ability.keybinding})"
+          expect(ability.dps.present?).to be true
+        end
     end
   end
-
 end
