03f87ada930e6248b50f7727f2b3db091887a154
Brian Nicholls <nichol88@gmail.com>
2022-09-17 17:35:26 -0400

    add routes for public api

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,22 @@
 Rails.application.routes.draw do
-  root to: 'overwatch#index'
+  root 'overwatch#index'
   
   resources :abilities
 
-  # Teams
   get 'teams/counter', to: 'teams#counter'
-  # post 'teams/counter', to: 'teams#calculate_counters'
   resources :teams
-
   resources :roles
-
   resources :heros do 
     resources :abilities
   end
-
   resources :hard_counters
-  
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # public API routes
+  namespace :api do
+    namespace :v1 do
+      resources :heros,     only: [ :index, :show ]
+      resources :abilities, only: [ :index, :show ]
+      resources :teams,     only: [ :index, :show ]
+    end
+  end
 end
