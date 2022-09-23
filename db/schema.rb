c1d427d6d40f8bf7f6033889d481eee967b8973d
Brian Nicholls <nichol88@gmail.com>
2022-09-23 13:00:14 -0400

    add maps table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_22_233251) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_23_165734) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -158,6 +158,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_22_233251) do
     t.bigint "game_id", null: false
     t.index ["game_id"], name: "index_heros_on_game_id"
     t.index ["role_id"], name: "index_heros_on_role_id"
+  end
+
+  create_table "maps", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "roles", force: :cascade do |t|
