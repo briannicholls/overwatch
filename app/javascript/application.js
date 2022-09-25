7d7eb776f1b56d4a62ce435dfc799dc7afc36fe7
Brian Nicholls <nichol88@gmail.com>
2022-09-25 18:07:03 -0400

    remove bad imports

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "./controllers"
-// import "controllers"
-import "./components"
+// import "./controllers"
+import "controllers"
+// import "./components"
+import "components"
 
-import 'react'
-import 'react-dom'
+// import 'react'
+// import 'react-dom'
 
 console.log('loaded application.js')
