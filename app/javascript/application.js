fe922bffcd9f2fd227e8450dc3f5392b780c5fa8
Brian Nicholls <nichol88@gmail.com>
2022-09-25 17:18:21 -0400

    fix syntax

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "./controllers"
+// import "controllers"
 import "./components"
 
 import 'react'
 import 'react-dom'
 
-console.log('loaded application.js')import "controllers"
+console.log('loaded application.js')
