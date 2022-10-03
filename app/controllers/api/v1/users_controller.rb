5fc5836e094854857646f48638d193de061f565f
Brian Nicholls <nichol88@gmail.com>
2022-10-02 23:06:18 -0400

    improve users api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,26 +3,18 @@ class Api::V1::UsersController < ApplicationController
   protect_from_forgery with: :null_session
 
   def create
-    @user = User.find_or_create_by({
-      email:    user_params[:email]
+    @user = User.create({
+      email: user_params[:email],
+      password: user_params[:password],
+      password_confirmation: user_params[:password_confirmation]
     })
 
-    if @user.persisted?
-      unless @user.authenticate password: user_params[:password]
-        render json: "Incorrect password", status: :unauthorized
-        return
-      end
-    else
-      @user.update({
-        password: user_params[:password],
-        password_confirmation: user_params[:password_confirmation]
-      })
-    end
-
-    # render json: @user
-
     respond_to do |format|
-      format.json { render :show, status: :created, location: api_v1_users_url(@user) }
+      if @user.persisted?
+        format.json { render :show, status: :created, location: api_v1_users_url(@user) }
+      else
+        format.json { render json: @user.errors.full_messages.to_sentence, status: :unprocessable_entity }
+      end
     end
   end
 
