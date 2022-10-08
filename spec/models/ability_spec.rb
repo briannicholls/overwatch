3032cabe5060f85cfe3f8b6da2d64a4d816dca92
Brian Nicholls <nichol88@gmail.com>
2022-10-08 14:05:19 -0400

    add validation for 5 heros per team

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -3,13 +3,13 @@ require 'pry'
 
 RSpec.describe Ability, type: :model do
   context "for Abilities that deal damage"
-    @abilities = Ability.non_ultimates.damage_dealing
-    @abilities.each do |ability|
-      describe "#{ability.name}#dps" do
-        it "should calculate a valid DPS value" do
-          puts "Testing #{ability.hero.name}'s Ability: #{ability.name} (#{ability.keybinding})"
-          expect(ability.dps.present?).to be true
-        end
+  @abilities = Ability.non_ultimates.damage_dealing
+  @abilities.each do |ability|
+    describe "#{ability.name}#dps" do
+      it "should calculate a valid DPS value" do
+        puts "Testing #{ability.hero.name}'s Ability: #{ability.name} (#{ability.keybinding})"
+        expect(ability.dps.present?).to be true
+      end
     end
   end
 end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -9,11 +9,12 @@ RSpec.describe Team, type: :model do
       Team.all.all?(&:valid?)
     end
 
+    # can remove below - this is added to AR validation
     it "should have 5 heros" do
       all_teams_have_5_heroes = Team.all.all?{|team| team.heros.length == 5 }
       expect(all_teams_have_5_heroes).to be true
     end
-    
+
   end
 
 end
