226ef8cd905b57dd0bb42f0dd74b26ad8e50bd18
Brian Nicholls <nichol88@gmail.com>
2022-11-03 18:34:51 -0400

    remove old files

--- db/seeds/001_games.rb
+++
@@ -1,8 +0,0 @@
-games = [
-  { name: 'Overwatch' },
-  { name: 'Overwatch 2' }
-]
-
-games.each do |game|
-  Game.find_or_create_by(name: game[:name])
-end
--- db/seeds/002_roles.rb
+++
@@ -1,8 +0,0 @@
-roles = [
-  { name: 'Tank' },
-  { name: 'DPS' },
-  { name: 'Support' },
-]
-roles.each do |role|
-  Role.find_or_create_by( name: role[:name] )
-end
